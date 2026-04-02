0547e57a43bd6061596796a0475ac99c64ecc909
HarshPatel-2005 <harsh.patel011@outlook.com>
2026-04-02 14:40:36 -0400

    Create RecentList.java

---
+++ A3_249/src/service/RecentList.java
@@ -0,0 +1,26 @@
+package service;
+
+import java.util.LinkedList;;
+
+public class RecentList <T>{
+	
+	private LinkedList<T> list = new LinkedList<>();
+	private final int MAX_SIZE = 10;
+	
+	public void addRecent(T item) {
+		
+	}
+	
+	public void printRecent(int maxToShow) {
+		
+	}
+	
+	public int size() {
+		
+	}
+	
+	public boolean isEmpty() {
+		
+	}
+	
+}
